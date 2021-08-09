ee7c276c4e5c2f450a2882e6eaaad510c6d06da3
APdrone <amanpatwalfor@gmail.com>
2021-08-09 16:36:03 +0530

    Create Test2.java
    adding test2 file

---
+++ TestProjectGit/src/testing/Test2.java
@@ -0,0 +1,15 @@
+package testing;
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		int data1=10;
+		int data2=20;
+		int data3=0;
+		int data4=0;
+		
+		System.out.println(data1*data2*data3+data4);
+	}
+
+}
