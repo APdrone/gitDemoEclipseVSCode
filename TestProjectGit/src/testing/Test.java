c94842ca34702de4d0369a6de57f6f306d9ebfd0
APdrone <amanpatwalfor@gmail.com>
2021-08-09 12:26:25 +0530

    Update Test.java
    doing multiplication

--- TestProjectGit/src/testing/Test.java
+++ TestProjectGit/src/testing/Test.java
@@ -4,10 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		
-		int data1=10;
-		int data2=20;
+		int i1=10;
+		int i2=20;
 		
-		System.out.println(data1+data2);
+		System.out.println(i1*i2);
 	}
 
 }
