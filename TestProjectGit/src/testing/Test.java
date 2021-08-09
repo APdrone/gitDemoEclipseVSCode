b525d5d6e4a22d94165f43a7a4ca199c9afe1f2b
APdrone <amanpatwalfor@gmail.com>
2021-08-09 12:25:05 +0530

    Adding addition method

--- TestProjectGit/src/testing/Test.java
+++ TestProjectGit/src/testing/Test.java
@@ -1,4 +1,13 @@
 package testing;
 public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		int data1=10;
+		int data2=20;
+		
+		System.out.println(data1+data2);
+	}
 
 }
